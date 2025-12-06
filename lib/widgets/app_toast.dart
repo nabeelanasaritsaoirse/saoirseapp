6dbcf96b5770352ded93f17235c488651f4fb124
ShahirShan786 <shahirshan786@gmail.com>
2025-12-06 16:08:31 +0530

    changed all toast to logs

--- lib/widgets/app_toast.dart
+++ lib/widgets/app_toast.dart
@@ -1,18 +1,14 @@
-import 'package:fluttertoast/fluttertoast.dart';
-
-import '../constants/app_colors.dart';
+import 'dart:developer';
 
 void appToast({
   required String content,
   String title = 'Message',
   bool error = false,
 }) {
-  Fluttertoast.showToast(
-    msg: content,
-    toastLength: Toast.LENGTH_SHORT,
-    gravity: ToastGravity.BOTTOM,
-    backgroundColor: error ? AppColors.red : AppColors.green,
-    textColor: AppColors.white,
-    fontSize: 14.0,
-  );
+  final status = error ? "❌ ERROR" : "✅ SUCCESS";
+
+  log("$status | $title → $content");
 }
+
+
+
