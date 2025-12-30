cdafeabbe4486225ac85df5e4948326b689cc93e
SHADIN FAISAL MP <144899419+SHADINFAISALMP@users.noreply.github.com>
2025-12-30 13:32:11 +0530

    alignment fix

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -740,6 +740,7 @@ class _HomeScreenState extends State<HomeScreen> {
                           product.name,
                           fontSize: 13.sp,
                           maxLines: 2,
+                          textAlign: TextAlign.left,
                           overflow: TextOverflow.ellipsis,
                           fontWeight: FontWeight.w600,
                         ),
