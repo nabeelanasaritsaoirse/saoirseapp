6389e2bab98dc654a4cebed1a8b4d57ac6822b3a
Akashellath <akashchandran367@gmail.com>
2026-02-13 16:53:11 +0530

    Faqs support button

--- lib/screens/faqs/faq_controller.dart
+++ lib/screens/faqs/faq_controller.dart
@@ -4,6 +4,8 @@ import 'package:get/get.dart';
 
 import '../../models/faq_model.dart';
 import '../../services/faq_service.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class FaqController extends GetxController {
   final FaqService _service = FaqService();
@@ -28,16 +30,45 @@ class FaqController extends GetxController {
 
       faqList.assignAll(result);
     } catch (e) {
-      // Get.snackbar(
-      //   'Error',
-      //   e.toString(),
-      //   snackPosition: SnackPosition.BOTTOM,
-      // );
+     
       log(e.toString());
     } finally {
       isLoading.value = false;
     }
   }
+
+
+//------------contact support call-----------------
+Future<void> contactSupportCall() async {
+  try {
+    final String number = dotenv.env['CUSTOMER_SUPPORT_NUMBER'] ?? '918089458905';
+    final Uri phoneUri = Uri.parse('tel:$number');
+
+    await launchUrl(phoneUri);
+  } catch (e) {
+    log('Error launching dialer: $e');
+  }
+}
+
+  //------------contact support whatsapp-----------------
+  Future<void> openWhatsAppSupport() async {
+  try {
+    final String number = dotenv.env['CUSTOMER_SUPPORT_NUMBER'] ?? '918089458905';
+    const String message = "Hello, I need help regarding EPI services.";
+
+    final Uri whatsappUri = Uri.parse(
+      "https://wa.me/$number?text=${Uri.encodeComponent(message)}",
+    );
+
+    await launchUrl(
+      whatsappUri,
+      mode: LaunchMode.externalApplication,
+    );
+  } catch (e) {
+    log('Error launching WhatsApp: $e');
+  }
+}
+
 
   void toggleExpand(int index) {
     if (expandedIndex.value == index) {
@@ -50,4 +81,7 @@ class FaqController extends GetxController {
   void collapseAll() {
     expandedIndex.value = -1;
   }
+
+
+
 }
--- lib/screens/faqs/faqs.dart
+++ lib/screens/faqs/faqs.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import '/constants/app_assets.dart';
+import '/widgets/app_loader.dart';
 
 import '../../constants/app_colors.dart';
 import '../../constants/app_strings.dart';
@@ -21,115 +23,175 @@ class FaqScreen extends StatelessWidget {
         title: AppStrings.FAQsTitle,
         showBack: true,
       ),
-      body: Padding(
-        padding: EdgeInsets.all(16.w),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            appText(
-              'Know More About EPI',
-              fontSize: 20.sp,
-              fontWeight: FontWeight.w600,
-            ),
+      body: Obx(() {
+        if (controller.isLoading.value) {
+          return Center(
+            child: appLoader(),
+          );
+        }
 
-            SizedBox(height: 6.h),
-            appText(
-              'Curious about our services?\nHere’s everything you need to know before you start saving smartly.',
-              fontSize: 15.sp,
-              color: AppColors.mediumGray,
-              textAlign: TextAlign.start,
-            ),
+        return SingleChildScrollView(
+          padding: EdgeInsets.all(16.w),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              appText(
+                'Know More About EPI',
+                fontSize: 20.sp,
+                fontWeight: FontWeight.w600,
+              ),
 
-            SizedBox(height: 20.h),
+              SizedBox(height: 6.h),
 
-            /// FAQ LIST
-            Expanded(
-              child: Obx(() {
-                if (controller.isLoading.value) {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
+              appText(
+                'Curious about our services?\nHere’s everything you need to know before you start saving smartly.',
+                fontSize: 15.sp,
+                color: AppColors.mediumGray,
+                textAlign: TextAlign.start,
+              ),
 
-                return ListView.separated(
-                  itemCount: controller.faqList.length,
-                  separatorBuilder: (_, __) => SizedBox(height: 12.h),
-                  itemBuilder: (context, index) {
-                    final faq = controller.faqList[index];
+              SizedBox(height: 20.h),
 
-                    return GestureDetector(
-                      behavior: HitTestBehavior.opaque,
-                      onTap: () => controller.toggleExpand(index),
-                      child: Container(
-                        padding: EdgeInsets.all(14.w),
-                        decoration: BoxDecoration(
-                          color: AppColors.lightGrey,
-                          borderRadius: BorderRadius.circular(12.r),
-                        ),
-                        child: Obx(() {
-                          final isExpanded =
-                              controller.expandedIndex.value == index;
+              /// FAQ LIST
+              ListView.separated(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: controller.faqList.length,
+                separatorBuilder: (_, __) => SizedBox(height: 12.h),
+                itemBuilder: (context, index) {
+                  final faq = controller.faqList[index];
 
-                          return Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Row(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  appText(
-                                    (index + 1).toString().padLeft(2, '0'),
-                                    fontSize: 15.sp,
-                                    fontWeight: FontWeight.w600,
-                                  ),
-                                  SizedBox(width: 10.w),
-                                  Expanded(
-                                    child: appText(faq.question,
-                                        fontSize: 15.sp,
-                                        fontWeight: FontWeight.w500,
-                                        overflow: TextOverflow.ellipsis,
-                                        maxLines: 2,
-                                        textAlign: TextAlign.start),
-                                  ),
-                                  SizedBox(width: 8.w),
-                                  Icon(
-                                    isExpanded ? Icons.remove : Icons.add,
-                                    size: 22.sp,
-                                    color: AppColors.primaryColor,
-                                  ),
-                                ],
-                              ),
-
-                              /// ANSWER
-                              AnimatedSize(
-                                duration: const Duration(milliseconds: 200),
-                                curve: Curves.easeInOut,
-                                child: isExpanded
-                                    ? Padding(
-                                        padding: EdgeInsets.only(
-                                          left: 26.w,
-                                          top: 12.h,
-                                        ),
-                                        child: appText(
-                                          faq.answer,
-                                          fontSize: 13.sp,
-                                          color: AppColors.darkGray,
-                                          textAlign: TextAlign.start,
-                                        ),
-                                      )
-                                    : const SizedBox(),
-                              ),
-                            ],
-                          );
-                        }),
+                  return GestureDetector(
+                    behavior: HitTestBehavior.opaque,
+                    onTap: () => controller.toggleExpand(index),
+                    child: Container(
+                      padding: EdgeInsets.all(14.w),
+                      decoration: BoxDecoration(
+                        color: AppColors.lightGrey,
+                        borderRadius: BorderRadius.circular(12.r),
                       ),
-                    );
-                  },
-                );
-              }),
+                      child: Obx(() {
+                        final isExpanded =
+                            controller.expandedIndex.value == index;
+
+                        return Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                appText(
+                                  (index + 1).toString().padLeft(2, '0'),
+                                  fontSize: 15.sp,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                                SizedBox(width: 10.w),
+                                Expanded(
+                                  child: appText(
+                                    faq.question,
+                                    fontSize: 15.sp,
+                                    fontWeight: FontWeight.w500,
+                                    overflow: TextOverflow.ellipsis,
+                                    maxLines: 2,
+                                    textAlign: TextAlign.start,
+                                  ),
+                                ),
+                                SizedBox(width: 8.w),
+                                Icon(
+                                  isExpanded ? Icons.remove : Icons.add,
+                                  size: 22.sp,
+                                  color: AppColors.primaryColor,
+                                ),
+                              ],
+                            ),
+
+                            /// ANSWER
+                            AnimatedSize(
+                              duration: const Duration(milliseconds: 200),
+                              curve: Curves.easeInOut,
+                              child: isExpanded
+                                  ? Padding(
+                                      padding: EdgeInsets.only(
+                                        left: 26.w,
+                                        top: 12.h,
+                                      ),
+                                      child: appText(
+                                        faq.answer,
+                                        fontSize: 13.sp,
+                                        color: AppColors.darkGray,
+                                        textAlign: TextAlign.start,
+                                      ),
+                                    )
+                                  : const SizedBox(),
+                            ),
+                          ],
+                        );
+                      }),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+        );
+      }),
+      floatingActionButton: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          //--------- contact button--------------
+          forhelperSupportButton(
+            imagePath: AppAssets.support_contact,
+            onTap: () {
+              controller.contactSupportCall();
+            },
+          ),
+
+          SizedBox(height: 12.h),
+
+          //-------- whatsapp button--------------
+          forhelperSupportButton(
+            imagePath: AppAssets.whatsapp,
+            onTap: () {
+              controller.openWhatsAppSupport();
+            },
+          ),
+        ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+    );
+  }
+
+  Widget forhelperSupportButton({
+    required String imagePath,
+    required VoidCallback onTap,
+  }) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: 46.r,
+        width: 46.r,
+        decoration: const BoxDecoration(
+          shape: BoxShape.circle,
+          boxShadow: [
+            BoxShadow(
+              color: AppColors.grey,
+              blurRadius: 3,
+              spreadRadius: 2,
+              offset: Offset(0, 3),
             ),
           ],
+        ),
+        child: ClipOval(
+          child: Image.asset(
+            imagePath,
+            fit: BoxFit.contain,
+          ),
         ),
       ),
     );
   }
 }
+
+
+
