cb6a2b65bf8738c8b06149ef3ee628d5ae777dec
Deep.saoirse <deep.it.saoirse@gmail.com>
2026-02-17 16:49:00 +0530

    fix: delete info function calling issue

--- lib/screens/profile/profile_controller.dart
+++ lib/screens/profile/profile_controller.dart
@@ -66,7 +66,6 @@ class ProfileController extends GetxController {
     _setInitialCountryFromCode('+91');
     fetchWishlistCount();
     fetchUserProfile();
-    fetchDeleteInfo();
 
     super.onInit();
   }
